ff562635bac928b2f48a73e0788b86fe5093f76f
jsalicetti <jessicasalicetti@yahoo.com>
2024-06-28 10:26:29 -0400

    contain method

---
+++ LAB303.3.1/src/containMethod.java
@@ -0,0 +1,21 @@
+Public class ContainExample {
+        public static void main(String[] args) {
+            Boolean result;
+
+            // check if str1 contains "Java"
+            result = true;
+            System.out.println(result);  // true
+
+            // check if str1 contains "Python"
+            result = false;
+            System.out.println(result);  // false
+
+            // check if str1 contains ""
+            result = true;
+
+            System.out.println(result);  // true
+        }
+    }
+
+
+}
